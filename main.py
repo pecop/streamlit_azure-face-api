ccb03dd5667b2bc8cddc046eeed6495d8986cad8
pecop <back.to.the.future52@gmail.com>
2020-12-16 15:23:27 +0900

    Added a function in main.py and an image file.

--- main.py
+++ main.py
@@ -3,46 +3,51 @@ import requests
 from PIL import Image, ImageDraw
 import json
 import io
-				
-st.title('Face Recognition App')
 
-subscription_key = st.sidebar.text_input('Enter your API key for Azure Face API', type="password")
-	
-assert subscription_key
+def face_recognition():				
+	st.title('Face Recognition App')
 
-root_url = 'https://20201215-pecop.cognitiveservices.azure.com/'
-endpoint_url = 'face/v1.0/detect'
-face_api_url = root_url + endpoint_url 
+	subscription_key = st.sidebar.text_input('Enter your API key for Azure Face API', type="password")
+		
+	assert subscription_key
 
-headers = {
-	    'Content-Type': 'application/octet-stream',
-	    'Ocp-Apim-Subscription-Key': subscription_key
-	}
+	root_url = 'https://20201215-pecop.cognitiveservices.azure.com/'
+	endpoint_url = 'face/v1.0/detect'
+	face_api_url = root_url + endpoint_url 
 
-params = {
-	    'returnFaceId': 'true',
-	    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
-	}
+	headers = {
+		    'Content-Type': 'application/octet-stream',
+		    'Ocp-Apim-Subscription-Key': subscription_key
+		}
 
-uploaded_file = st.file_uploader('Choose an image.', type='jpg')
+	params = {
+		    'returnFaceId': 'true',
+		    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+		}
 
-if uploaded_file is not None:
-	img = Image.open(uploaded_file)
+	uploaded_file = st.file_uploader('Choose an image.', type='jpg')
 
-	with io.BytesIO() as output:
-	    img.save(output, format='JPEG')
-	    binary_img = output.getvalue()
+	if uploaded_file is not None:
+		img = Image.open(uploaded_file)
 
-	try:
-		res = requests.post(face_api_url, params=params, headers=headers, data=binary_img)
-	except Exception as e:
-		st.sidebar.warning('Your API key is invalid.')
+		with io.BytesIO() as output:
+		    img.save(output, format='JPEG')
+		    binary_img = output.getvalue()
 
-	results = res.json()
+		try:
+			res = requests.post(face_api_url, params=params, headers=headers, data=binary_img)
+		except Exception as e:
+			st.sidebar.warning('Your API key is invalid.')
 
-	for result in results:
-	    rect = result['faceRectangle']
-	    draw = ImageDraw.Draw(img)
-	    draw.rectangle([(rect['left'], rect['top']), (rect['left']+rect['width'], rect['top']+rect['height'])], fill=None, outline='gold', width=10)
+		results = res.json()
 
-	st.image(img, caption='Uploaded Image', use_column_width=True)
+		for result in results:
+		    rect = result['faceRectangle']
+		    draw = ImageDraw.Draw(img)
+		    draw.rectangle([(rect['left'], rect['top']), (rect['left']+rect['width'], rect['top']+rect['height'])], fill=None, outline='gold', width=10)
+
+		st.image(img, caption='Uploaded Image', use_column_width=True)
+
+
+if __name__ == '__main__':
+	face_recognition()
